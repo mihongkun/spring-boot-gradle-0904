31a6ad6be168ab501a4c1d53009e283c9cfbcad0
mihk <mihongkun@gmail.com>
2017-09-04 21:21:17 +0800

    add di

--- chapter01/src/main/java/com/mihk/di/DiConfig.java
+++ chapter01/src/main/java/com/mihk/di/DiConfig.java
@@ -1,4 +1,12 @@
 package com.mihk.di;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.mihk.di")
 public class DiConfig {
+
+
+
 }
---
+++ chapter01/src/main/java/com/mihk/di/Main.java
@@ -0,0 +1,14 @@
+package com.mihk.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext  context = new AnnotationConfigApplicationContext(DiConfig.class);
+        UseFunctionService useFunctionService = context.getBean(UseFunctionService.class);
+        System.out.println(useFunctionService.sayHello("world!"));
+        context.close();
+
+    }
+}
