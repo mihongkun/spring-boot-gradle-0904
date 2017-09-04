2655e05c0a4e9dd84c413eccf8201204357e2bca
mihk <mihongkun@gmail.com>
2017-09-05 07:35:29 +0800

    java config

---
+++ chapter01/src/main/java/com/mihk/javaconfig/UseFunctionService.java
@@ -0,0 +1,14 @@
+package com.mihk.javaconfig;
+
+public class UseFunctionService {
+    FunctionService functionService;
+
+    public void setFunctionService(FunctionService functionService) {
+        this.functionService = functionService;
+    }
+
+    public String sayHello(String word){
+
+        return functionService.sayHello(word);
+    }
+}
